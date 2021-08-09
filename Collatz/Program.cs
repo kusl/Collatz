08376e202c417bf83c469d460e3cd3c4e8535e2c
kushal <kushaldeveloper@gmail.com>
2021-08-09 15:56:33 -0600

    Add project files.

---
+++ Collatz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collatz
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<UInt64, int> dictionary= new Dictionary<UInt64, int>();
+            UInt64 max = UInt64.MaxValue;
+            for (UInt64 input = 1; input < max; input++)
+            {
+                int result = Collatz(input);
+                dictionary.Add(input, result);
+            }
+        }
+        static UInt64 IfEven(UInt64 input)
+        {
+            return input / 2;
+        }
+        static UInt64 IfOdd(UInt64 input)
+        {
+            return 3 * input + 1;
+        }
+        static int Collatz(UInt64 input)
+        {
+            UInt64 originalInput = input;
+            Console.WriteLine(input);
+            int counter = 0;
+            while (input > 1)
+            {
+                counter++;
+                if (input % 2 == 0)
+                {
+                    input = IfEven(input);
+                }
+                else
+                {
+                    input = IfOdd(input);
+                }
+            }
+            Console.WriteLine($"My calculation for Collatz counter for {originalInput} is {counter}.");
+            return counter;
+        }
+    }
+}
